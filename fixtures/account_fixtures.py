b721493a2d757dfa54e9a664199c950e8f4b3392
evgeniy_vladimirov <vladimiroves.qa@gmail.com>
2024-02-17 12:52:26 +0300

    add link for job

--- fixtures/account_fixtures.py
+++ fixtures/account_fixtures.py
@@ -13,10 +13,10 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.fixture()
-def create_user_for_login(base_url):
+def create_user_for_login():
     user = UserData.user_changes
-    user.userId = ApiFacade(base_url=base_url).account.create_user(user)['userID']
+    user.userId = ApiFacade().account.create_user(user)['userID']
     yield user
-    user.token = AccountApi(base_url=base_url, module='Account').generate_token(user=user)[
+    user.token = AccountApi(module='Account').generate_token(user=user)[
         'token']
-    ApiFacade(base_url=base_url, auth_token=user.token).account.delete_user(user)
+    ApiFacade(auth_token=user.token).account.delete_user(user)
